1a29a3443f19ddba35d780c8f62abc6e04b1ba48
kendraKohler <kohler.kendra@gmail.com>
2018-03-28 12:33:53 -0700

    debug, printing full post model

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -30,9 +30,11 @@ class PostController
 
     	$postModel = $this->postDatabase->getPostById($id);
 
-    	error_log("POST MODEL TITLE: ".$postModel->title());
-    	error_log("POST MODEL BODY: ".$postModel->body());
-    	error_log("POST MODEL AUTHOR NAME: ".$postModel->author()->fullName());
+    	error_log('POST MODEL: '.print_r($postModel,1));
+
+    	//error_log("POST MODEL TITLE: ".$postModel->title());
+    	//error_log("POST MODEL BODY: ".$postModel->body());
+    	//error_log("POST MODEL AUTHOR NAME: ".$postModel->author()->fullName());
 
         return $this->templating->renderResponse('default/postId.html.twig',[
         	'title' => $postModel->title(),
