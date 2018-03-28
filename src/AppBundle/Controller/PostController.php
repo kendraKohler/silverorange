3e8b440aae614af0891c97fc2b2aac9c7f3e6467
kendraKohler <kohler.kendra@gmail.com>
2018-03-28 00:38:22 -0700

    adding route with controller

---
+++ src/AppBundle/Controller/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+
+class PostController
+{
+	private $templating;
+
+	public function __construct(EngineInterface $templating)
+	{
+		$this->templating = $templating;
+	}
+
+    public function indexAction(Request $request)
+    {
+        return $this->templating->renderResponse('default/post.html.twig');
+    }
+}
