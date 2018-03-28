ab1df5ccd205316da924599f00487aaa7a977ca5
kendraKohler <kohler.kendra@gmail.com>
2018-03-28 14:55:58 -0700

    allowing importer to take array, commenting code

--- src/AppBundle/Controller/ImportController.php
+++ src/AppBundle/Controller/ImportController.php
@@ -17,28 +17,50 @@ class ImportController
 		$this->postDatabase = $postDatabase;
 	}
 
+	//Gets posted data and saves to the database
     public function indexAction(Request $request)
     {
-    	// Get json request data as object
+    	// Get json request data as object, otherwise save normally
     	$jsonData = $request->getContent();
 
-    	$postData = json_decode($jsonData);
+		//Loop through data if array is sent
+    	if(is_array($jsonData))
+    	{
+    		foreach($jsonData as $currentData)
+    		{
+    			$this->saveToDatabase($currentData);
+    		}
+    	}
+    	else
+    	{
+    		$this->saveToDatabase($jsonData);
+    	}
 
-    	// Create post model using json data
-    	$postModel = new PostModel();
+    }
 
-    	//Issue with symfony constructor work around
-    	$postModel->initialize(
-    		$postData->id,
-    		$postData->title,
-    		$postData->body,
-    		$postData->created_at,
-    		$postData->modified_at,
-    		$postData->author);
+	// Decode json data, store in model and save to database
+    private function saveToDatabase($data)
+    {
+    	//Decode json data
+		$postData = json_decode($data);
 
-    	// Save to database
-    	$this->postDatabase->savePost($postModel);
+		if($postData)
+		{
+			// Create post model using json data
+	    	$postModel = new PostModel();
 
+	    	//Issue with symfony constructor work around
+	    	$postModel->initialize(
+	    		$postData->id,
+	    		$postData->title,
+	    		$postData->body,
+	    		$postData->created_at,
+	    		$postData->modified_at,
+	    		$postData->author);
+
+	    	// Save to database
+	    	$this->postDatabase->savePost($postModel);
+		}
     }
 }
 
