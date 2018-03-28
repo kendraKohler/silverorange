f73455f38dc8961000a2d4ee5348131d1ef6e273
kendraKohler <kohler.kendra@gmail.com>
2018-03-28 09:01:31 -0700

    getting all post joined with author data and adding aliases so it is clear where the data is from

--- src/AppBundle/Database/PostDatabase.php
+++ src/AppBundle/Database/PostDatabase.php
@@ -41,8 +41,23 @@ class PostDatabase
 
     public function getPostById($id)
     {
-        //Get post and it's author data by id
-        $stmt = $this->connection->query('SELECT * FROM posts p JOIN authors a ON p.author = a.id');
+        /*
+         * Get post and it's author data by id
+         * NOTE : although all data is not needed for this task, 
+         * all data is grabbed so the model will contain everything
+         * for any future needs.
+         */
+        $stmt = $this->connection->query('
+            SELECT p.id AS post_id, 
+                p.title AS post_title, 
+                p.body AS post_body, 
+                p.created_at AS post_created_at, 
+                p.modified_at AS post_modified_at, 
+                a.id AS author_id, 
+                a.full_name AS author_full_name,
+                a.created_at AS author_created_at,
+                a.modified_at AS author_modified_at
+            FROM posts p JOIN authors a ON p.author = a.id');
         $row = $stmt->fetchAll();
 
         error_log('ROW DATA FETCHED ON JOIN: ' . print_r($row,1));
