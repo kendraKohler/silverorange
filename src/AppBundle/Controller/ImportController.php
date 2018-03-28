ed631da44fbdf5c4f0cd1623a2b20e458904eaf2
kendraKohler <kohler.kendra@gmail.com>
2018-03-28 01:05:47 -0700

    adding in missing bracket

--- src/AppBundle/Controller/ImportController.php
+++ src/AppBundle/Controller/ImportController.php
@@ -11,6 +11,6 @@ class ImportController
     public function indexAction(Request $request)
     {
     	$jsonData = $request->getContent();
-        error_log('JSON DATA IS: '.print_r($jsonData,1);
+        error_log('JSON DATA IS: '.print_r($jsonData,1));
     }
 }
